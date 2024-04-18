413c978ec87f0e737a7ca8533eafc17f514156d9
pinedalaraaa <lbpineda1@up.edu.ph>
2024-04-18 12:06:55 +0800

    chore: updated app icon

--- lib/shared/checkbox_card/options.dart
+++ lib/shared/checkbox_card/options.dart
@@ -1,25 +1,112 @@
 // options to display inside checkbox cards
 
 class Options {
-  static const List<String> dietaryPreferences = [
-    'Keto',
-    'High-Protein',
-    'Low-Carb',
-    'Low-Sodium',
-    'Low-Fat',
-    'Pescetrian',
-    'Pork-free',
-    'Kosher',
-    'Alcohol-free',
-    'No-Sugar',
-    'Dummy',
-    'Dummy',
-    'Dummy',
-    'Dummy',
-    'Dummy',
+  static const List<String> kitchenEquipments = [
+    // Choices for another card
   ];
 
-  static const List<String> otherCardChoices = [
-    // Choices for another card
+  static const List<String> dietaryPreferences = [
+    'Alcohol-free',
+    'Balanced',
+    'High-Fiber',
+    'High-Protein',
+    'Keto',
+    'Kidney friendly',
+    'Kosher',
+    'Low-Carb',
+    'Low-Fat',
+    'Low potassium',
+    'Low-Sodium',
+    'No oil added',
+    'No-sugar',
+    'Paleo',
+    'Pescatrian',
+    'Pork-free',
+    'Red meat-free',
+    'Sugar-conscious',
+    'Vegan',
+    'Vegetarian',
+  ];
+
+  static const List<String> allergies = [
+    'Celery-free',
+    'Crustacean-free',
+    'Dairy-free',
+    'Egg-free',
+    'Fish-free',
+    'Gluten-free',
+    'Lupine-free',
+    'Mustard-free',
+    'Peanut-free',
+    'Sesame-free',
+    'Shellfish-free',
+    'Soy-free',
+    'Tree-Nut-free',
+    'Wheat-free',
+  ];
+
+  static const List<String> macronutrients = [
+    'Fat',
+    'Saturated',
+    'Trans',
+    'Monounsaturated',
+    'Polyunsaturated',
+    'Carbs',
+    'Fiber',
+    'Sugars',
+    'Protein',
+  ];
+
+  static const List<String> micronutrients = [
+    'Cholesterol',
+    'Calcium',
+    'Iron',
+    'Folate (Equivalent)',
+    'Magnesium',
+    'Niacin (B3)',
+    'Phosphorus',
+    'Potassium',
+    'Riboflavin (B2)',
+    'Thiamin (B1)',
+    'Sodium',
+    'Vitamin A',
+    'Vitamin B6',
+    'Vitamin B12',
+    'Vitamin C',
+    'Vitamin D',
+    'Vitamin E',
+    'Vitamin K'
+  ];
+
+  static const List<String> pantryEssentials = [
+    'Butter',
+    'Sugar',
+    'Egg',
+    'Garlic',
+    'Onion',
+    'Vanilla',
+    'Soy Sauce',
+    'Flour',
+    'Honey',
+    'Cinnamon',
+  ];
+
+  static const List<String> meat = [
+    'Bacon',
+    'Bacon bits',
+    'Ground beef',
+    'Pork chops',
+    'Pork loin',
+    'Chicken breast',
+    'Chicken thighs',
+    'Chicken drumsticks',
+    'Chicken wings',
+    'Pork belly',
+  ];
+
+  static const List<String> vegetables = [
+    'Carrot',
+    'Potato',
+    'Tomato',
   ];
 }
