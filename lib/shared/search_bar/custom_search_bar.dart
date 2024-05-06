5f19d3e2e15823aaeaa0e5e0b0bc6cde114d8133
pinedalaraaa <lbpineda1@up.edu.ph>
2024-05-07 00:16:59 +0800

    chore: moved custom search  bar widget from lib/core/widgets to lib/shared/search-bar

---
+++ lib/shared/search_bar/custom_search_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchBar extends StatefulWidget {
+  final Function(String) onSubmitted;
+
+  const CustomSearchBar({
+    super.key,
+    required this.onSubmitted,
+  });
+
+  @override
+  State<CustomSearchBar> createState() => _CustomSearchBarState();
+}
+
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.all(16),
+        child: TextField(
+            controller: _controller,
+            onSubmitted: widget.onSubmitted,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white60,
+              labelText: 'Search',
+              border: OutlineInputBorder(),
+
+              // search icon
+              prefixIcon: Icon(Icons.search),
+              prefixIconColor: Colors.blueGrey,
+
+              // filter icon
+              suffixIcon: Icon(Icons.filter_alt_outlined),
+              suffixIconColor: Colors.blueGrey,
+            )));
+  }
+}
